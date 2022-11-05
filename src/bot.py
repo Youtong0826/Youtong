ab10b3da856c6d053f0240f808545ed3032f286e
Youtong0826 <jimmy2007826@gmail.com>
2022-11-05 18:31:34 +0800

    創建bot

---
+++ src/bot.py
@@ -0,0 +1,17 @@
+import discord
+
+from core.classes import Bot
+
+bot = Bot(
+    intents = discord.Intents.all(),
+    setting_path = "setting.json",
+    command_prefix ="n!"
+)
+
+@bot.event
+async def on_ready():
+    print("bot is ready!!")
+
+if __name__ == "__main__":
+    bot.setup()
+    bot.run(bot.token)
