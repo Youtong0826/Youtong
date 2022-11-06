3cb5564db47f9705b6b236f383655e966ec8f6ad
Youtong0826 <jimmy2007826@gmail.com>
2022-11-06 18:24:02 +0800

    add new classes and functions

--- src/core/classes.py
+++ src/core/classes.py
@@ -7,7 +7,8 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 from typing import (
-    Any
+    Any,
+    List
 )
 
 from core.functions import (
@@ -18,8 +19,68 @@ from core.functions import (
 
 load_dotenv()
 
+class Database(Database):
+    def __init__(self, path: str, db_name: str = "__default__", auto_init: Any = None) -> None:
+        super().__init__(path, db_name, auto_init)
+
+        self.data = {
+            "block_roles":self.block_roles,
+            "block_words":self.block_words
+        }
+
+    def append(self, key, more ,mode="int") -> any:
+        """
+        if mode `int` that will add more to the value of key. 
+        if mode `list` that will append new obj to the value(list) of key
+        """
+        if mode == "int":
+            return super().append(key, more) 
+
+        elif mode == "list":
+            data = self.get(key)
+            data.append(more)
+
+        return self.set(key,data)
+            
+    def reset(self) -> None:
+        "reset all the value "
+        return [self.set(key,None) for key in self.get_all_key()]
+
+    def set_block_roles(self, new:List[discord.Role]) :
+        "set the new value to the `block_roles`"
+        
+        return None if not self.get("block_roles") else (self.set("block_roles", new) if isinstance(new, list) else None)
+
+    def set_block_words(self, new:List[discord.Role]):
+        "set the new value to the `block_words`"
+
+        return None if not self.get("block_words") else (self.set("block_words", new) if isinstance(new, list) else None)
+
+    def append_block_roles(self, role:discord.Role) -> None:
+        "append the value to the `block_roles`"
+
+        return None if not self.get("block_roles") else self.append("block_words", role)
+
+    def append_block_words(self, channel:discord.abc.GuildChannel)  -> None:
+        "append the value to the `block_words`"
+        return None if not self.get("block_words") else self.append("block_words", channel)
+
+    @property
+    def block_roles(self) -> list:
+        if not self.get("block_roles"):
+            self.set("block_roles",[])
+
+        return self.get("block_roles")
+        
+    @property
+    def block_words(self) -> list:
+        if not self.get("block_words"):
+            self.set("block_words",[])
+
+        return self.get("block_words")
+
 class Setting:
-    def __init__(self,path:str) -> None:
+    def __init__(self, path:str) -> None:
         self._setting:dict = read_json(path)
         self.database = self._setting.get("database",{})
         self.general = self._setting.get("general",{})
@@ -28,16 +89,20 @@ class Setting:
         self.path = path
 
     def add_category(self, name:str, default:dict={}):
+        "add new category"
         return write_json(self.path, name ,default)
 
     def add_admin(self, id:int):
+        "add new admin(id)"
         self.admins.append(id)
         return self.add_category("managements", {"admin":self.admins})
 
     def add_cog(self, key:str, value:Any):
-        return self.add_category("cog",{ key:value})
+        "add new data at the cog"
+        return self.add_category("cog", {key:value})
 
     def add_database(self, key:str, value:Any):
+        "add new data at the database"
         return self.add_category("database", {key:value})
 
 class Bot(commands.Bot):
@@ -71,10 +136,11 @@ class Bot(commands.Bot):
             await ctx.reply("reloading end!")
 
     def setup(self):
+        "setup the bot"
         return [load_extension(self,folder) for folder in self.setting.cog["folder"]]
 
 class CogExtension(discord.Cog):
-    def __init__(self,bot : Bot) -> None:
+    def __init__(self,bot:Bot) -> None:
         self.bot = bot
 
 if __name__ == "__main__":
--- src/core/functions.py
+++ src/core/functions.py
@@ -1,6 +1,17 @@
 import discord
 import json
 import os
+import re
+
+def is_emoji(content:str):
+    emoji_regexp = u'[\U00002600-\U000026FF]|[\U00002700-\U000027BF]|[\U0001f300-\U0001f5fF]|[\U0001f600-\U0001f64F]|[\U0001f680-\U0001f6FF]'
+    result = []
+    for chr in content:
+        result += re.findall(emoji_regexp, chr, re.UNICODE)
+        
+    result += re.findall("<:\w+:\d+>|<\w+:\w+:\d+>",content)
+
+    return False if len(result) == 0 else True
 
 def load_extension(bot:discord.Bot, folder:str, mode:str="load", is_notice:bool=True) -> None:
 
@@ -46,3 +57,16 @@ def write_json(path:str, key:str, *value:dict, is_log:bool = True):
             separators=(",",":"),
             ensure_ascii=False
         ))
+
+if __name__ == "__main__":
+    def test():                     #1038746334624231505
+        print(is_emoji("<:Tenshi_cry:991664784682520667>"))
+        print(is_emoji("<a:awoothink:9991059835047240500>"))
+        print(is_emoji("hihi 😂🕐"))
+        print(is_emoji("hihi"))
+
+    def string_to_list(string):
+        return str(list(string)).replace("'",'"')
+
+    print(string_to_list("ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄔㄕㄖㄗㄘㄙㄧㄨㄩㄞㄛㄜㄝㄢㄟㄠㄡㄦㄣㄤㄥ"))
+    print(string_to_list("0123456789"))
