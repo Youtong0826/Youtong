e8056585c6466a67aa2ff0c4f68142555e52184b
Youtong0826 <jimmy2007826@gmail.com>
2022-11-20 02:39:04 +0800

    add functions

--- src/core/functions.py
+++ src/core/functions.py
@@ -1,17 +1,8 @@
 import discord
 import json
 import os
-import re
 
-def is_emoji(content:str):
-    emoji_regexp = u'[\U00002600-\U000026FF]|[\U00002700-\U000027BF]|[\U0001f300-\U0001f5fF]|[\U0001f600-\U0001f64F]|[\U0001f680-\U0001f6FF]'
-    result = []
-    for chr in content:
-        result += re.findall(emoji_regexp, chr, re.UNICODE)
-        
-    result += re.findall("<:\w+:\d+>|<\w+:\w+:\d+>",content)
-
-    return False if len(result) == 0 else True
+from datetime import datetime, timezone, timedelta
 
 def load_extension(bot:discord.Bot, folder:str, mode:str="load", is_notice:bool=True) -> None:
 
@@ -46,10 +37,10 @@ def read_json(path:str) -> dict:
     with open(path, "r", encoding="utf-8") as file:
         return json.loads(file.read())
 
-def write_json(path:str, key:str, *value:dict, is_log:bool = True):
-    new = original = read_json(path)
+def write_json(path:str, key:str, *value:dict, keeping:bool =True, is_log:bool = True):
+    new = original = read_json(path) if keeping else {}
     new[key] = merge_dict(original.get(key,{}),*value)
-    
+
     with open(path, "w", encoding="utf-8") as file:
         return file.write(json.dumps(
             new,
@@ -58,15 +49,10 @@ def write_json(path:str, key:str, *value:dict, is_log:bool = True):
             ensure_ascii=False
         ))
 
+def now(hours=8):
+    return datetime.now(timezone(timedelta(hours=hours)))
+
 if __name__ == "__main__":
-    def test():                     #1038746334624231505
-        print(is_emoji("<:Tenshi_cry:991664784682520667>"))
-        print(is_emoji("<a:awoothink:9991059835047240500>"))
-        print(is_emoji("hihi 😂🕐"))
-        print(is_emoji("hihi"))
 
     def string_to_list(string):
         return str(list(string)).replace("'",'"')
-
-    print(string_to_list("ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄔㄕㄖㄗㄘㄙㄧㄨㄩㄞㄛㄜㄝㄢㄟㄠㄡㄦㄣㄤㄥ"))
-    print(string_to_list("0123456789"))
